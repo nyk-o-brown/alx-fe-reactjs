21a835954fe3a1360cac1c4630f1a4a5e7518880
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-09 23:40:40 +0300

    daktr

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,5 +5,13 @@ export const useRecipeStore = create(set => ({
   addRecipe: (recipe) => set(state => ({
     recipes: [...state.recipes, recipe]
   })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (id) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe => 
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+  }))
 }));
