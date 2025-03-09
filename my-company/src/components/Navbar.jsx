6e3487f9a27f3d7fd0d77d51e544cfcbd3559831
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-09 22:04:53 +0300

    navbarstyling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/home">Home</Link>
+        <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+            <ul style={{ display: 'flex', justifyContent: 'center', listStyle: 'none', margin: 0, padding: 0 }}>
+                <li style={{ margin: '0 10px' }}>
+                    <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
                 </li>
-                <li>
-                    <Link to="/about">About</Link>
+                <li style={{ margin: '0 10px' }}>
+                    <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
                 </li>
-                <li>
-                    <Link to="/services">Services</Link>
+                <li style={{ margin: '0 10px' }}>
+                    <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
                 </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
+                <li style={{ margin: '0 10px' }}>
+                    <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
                 </li>
             </ul>
         </nav>
