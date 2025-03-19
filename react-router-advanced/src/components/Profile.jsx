5e647aee61c86c8d51ff4e4f96b73cc2733bc3af
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-19 19:41:06 +0300

    cmt de nested routes link

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,26 @@
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
     <div>
-      <h1>Profile</h1>
+      <h1>Profile Page</h1>
+
+      {/* Navigation Links */}
       <nav>
-        <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
+        <Link to="details" style={{ marginRight: "10px" }}>
+          Profile Details
+        </Link>
+        <Link to="settings">Profile Settings</Link>
       </nav>
-      {/* This is where nested routes will render their components */}
-      <Outlet />
+
+      {/* Nested Routes for Profile Sections */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 };
