d236cdfb1d5de692638c5c8f6b5d2532531a57db
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-20 19:36:11 +0300

    cmt of profile

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -6,44 +6,48 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import { ProfileDetails, ProfileSettings } from "./components/ProfileDetails";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./Auth";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Router>
-      <>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
+    <AuthProvider>
+      <Router>
+        <>
+          <div>
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
           </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-        <Routes>
-          <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="profile" element={<Profile />}>
-            {/* Nested routes for Profile subsections */}
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
-          <ProtectedRoute path="/blog/:id" element={<BlogPost />} /> {/* Dynamic Route */}
-        </Routes>
-      </>
-    </Router>
+          <Routes>
+            <Route path="/" element={<h1>Home Page</h1>} />
+            <Route path="profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}>
+              {/* Nested routes for Profile subsections */}
+              <Route path="details" element={<ProfileDetails />} />
+              <Route path="settings" element={<ProfileSettings />} />
+            </Route>
+            <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic Route */}
+          </Routes>
+        </>
+      </Router>
+    </AuthProvider>
   );
 }
 
