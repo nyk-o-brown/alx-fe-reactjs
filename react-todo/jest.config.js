4c6187c305a5736dec0240a80584e7fe75ba558b
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-21 17:33:49 +0300

    cmt el tust is running

--- react-todo/jest.config.js
+++ react-todo/jest.config.js
@@ -1,13 +1,8 @@
-// Ensure the code runs in a Node.js environment
-if (typeof module === 'undefined') {
-  var module = {};
-}
-
-module.exports = {
+export default {
   // Map CSS (or other style files) to our mock file
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+    '\\.(jpg|jpeg|png|gif|webp|svg|eot|otf|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
   },
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest'
