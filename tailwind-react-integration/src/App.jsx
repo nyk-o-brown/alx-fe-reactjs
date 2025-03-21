ae52999c55d60881f276a14ac29c09420ab87079
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-21 20:48:38 +0300

    cmt el me 2

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,19 +1,14 @@
 import { useState } from 'react'
-
-import './index.css'; // Import Tailwind CSS here
-import UserProfile from './components/UserProfile';
+import './index.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
-
-    <div className='min-h-screen bg-gray-300'>
-
+    <div className="min-h-screen bg-gray-200">
       <UserProfile />
- 
     </div>
-
   )
 }
 
