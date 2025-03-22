418ff82611808ddd8a9ea1c2b9f706052a1326ac
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-22 20:38:55 +0300

    cmt ot me imptnt

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // You can set it to 'media' or 'class' if needed
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
