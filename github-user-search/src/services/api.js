ab9184d5670e6d6bc8713d521f7fe3d97c2cd53e
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-27 22:31:45 +0300

    cmt an vercel shit

--- github-user-search/src/services/api.js
+++ github-user-search/src/services/api.js
@@ -1,9 +1,12 @@
-// services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https:/github.com/api',
+  baseURL: 'https://api.github.com',
+  headers: {
+    'Accept': 'application/vnd.github.v3+json',
+    'Authorization': `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
+  }
 });
 
-export const getUsers = () => api.get('/users');
+export const getUsers = () => api.get('/search/users');
 export const createUser = (userData) => api.post('/users', userData);
